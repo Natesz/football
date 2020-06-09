e6cee018a89346bc0753699c59b7e08a4d355e02
Natesz <natesz777@yahoo.com>
2020-06-09 17:50:34 +0200

    Updates

---
+++ precache-manifest.720c194ab50bf02dade25c5e8c7078e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ade13423c1849e5075fc7250de95e3b",
+    "url": "/football/index.html"
+  },
+  {
+    "revision": "cb4efc3fb11f835df97e",
+    "url": "/football/static/css/main.af3d18fb.chunk.css"
+  },
+  {
+    "revision": "eb025d8f320b770ecde4",
+    "url": "/football/static/js/2.deb41f83.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/football/static/js/2.deb41f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4efc3fb11f835df97e",
+    "url": "/football/static/js/main.70cca457.chunk.js"
+  },
+  {
+    "revision": "e4946af12e4b71c4e66f",
+    "url": "/football/static/js/runtime-main.bae08913.js"
+  }
+]);
